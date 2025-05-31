6142334bf95437f13bed7c2fbd0285b872a28b99
Satendra Rajput <satendracaria@gmail.com>
2025-05-31 18:01:42 +0530

    pre-render3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { serverRoutes } from './app.routes.server';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), 
-    serverRoutes,
     provideClientHydration(),
     // Enable event replay for client-side hydration
     provideClientHydration(withEventReplay())
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+    {
+    path: 'projects/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      return Promise.resolve([
+        { id: '1' },
+        { id: '2' },
+        { id: '3' }
+      ]);
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
