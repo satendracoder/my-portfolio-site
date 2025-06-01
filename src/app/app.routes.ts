e728f7b69d9d12c2a50bdb79181fd545cccf38ab
Satendra Rajput <satendracaria@gmail.com>
2025-06-01 15:04:39 +0530

    Portfolio Menu chang1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,33 +12,38 @@ export const routes: Routes = [
         children: [
             {
                 path: 'about',
+                title: 'About Us - Satendra Rajput',
                 loadComponent: () => import('./pages/home/about-us/about-us').then(m => m.AboutUs)
             },
             {
                 path: 'experience',
+                title: 'Experience - Satendra Rajput',
                 loadComponent: () => import('./pages/home/experience/experience').then(m => m.Experience)
             },
             {
                 path: 'contact',
+                title: 'Contact - Satendra Rajput',
                 loadComponent: () => import('./pages/home/contact/contact').then(m => m.Contact)
             },
             {
                 path: 'projects',
+                title: 'Projects - Satendra Rajput',
                 loadComponent: () => import('./pages/home/project/project').then(m => m.Project)
             }
             ,
             {
                 path: 'projects/:id',
+                title: 'Project Details - Satendra Rajput',
                 loadComponent: () => import('./pages/home/project-details/project-details')
                     .then(m => m.ProjectDetails),
-    data: {
-        renderMode: 'pre-render',
-        getPrerenderParams: () => [
-            { id: '1' },
-            { id: '2' },
-            { id: '3' }
-        ]
-    },
+                data: {
+                    renderMode: 'pre-render',
+                    getPrerenderParams: () => [
+                        { id: '1' },
+                        { id: '2' },
+                        { id: '3' }
+                    ]
+                },
             }
         ]
     },
