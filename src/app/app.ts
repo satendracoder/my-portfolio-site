093b980b9eabd2c4906a5531c58b3437c5f6e792
Satendra Rajput <satendracaria@gmail.com>
2025-06-30 14:25:08 +0530

    Create BTN_pro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'satendra-sde';
@@ -32,6 +32,13 @@ export class App {
         this.deferredPrompt = null;
         this.showInstallButton = false;
       });
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'i') {
+      this.installApp();
     }
   }
 }
