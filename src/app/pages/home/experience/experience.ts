cbbd404732097c9d2fc3ab3dae679827ee5dcbe5
Satendra Rajput <satendracoder@gmail.com>
2025-11-14 14:10:16 +0530

    Update

--- src/app/pages/home/experience/experience.ts
+++ src/app/pages/home/experience/experience.ts
@@ -14,42 +14,41 @@ interface Experience_1 {
   selector: 'app-experience',
   imports: [],
   templateUrl: './experience.html',
-  styleUrl: './experience.scss'
+  styleUrl: './experience.scss',
 })
 export class Experience {
-
-   constructor(private meta: Meta, private titleService: Title) {}
+  constructor(private meta: Meta, private titleService: Title) {}
 
   ngOnInit(): void {
     this.setMetaTags();
   }
 
-  companies:any = [
-    {
-      name: 'Fine Quick Tech Pvt Ltd',
-      icon: 'https://www.finequick.com/favicon.svg',
-      duration: 'July 2025 - Present (Part-time)',
-      degination: "Software Engineer",
-      location: 'Greate Noida, Uttar Pradesh · Remote',
-      points: [
-        'Designed and developed the frontend interface using Angular with modular, component-driven architecture.',
-        'Created REST APIs using Spring Boot for dynamic tool rendering and processing.',
-        'Built interactive forms for input-heavy tools like plagiarism checkers, converters, and calculators.',
-        'Integrated third-party APIs (OCR, Translation, SEO APIs) with secure handling.',
-        'Managed file upload/download systems for tools like PDF converters and image converters.',
-        'Implemented JWT-based authentication for secure sections of the platform.',
-        'Implemented JWT-based authentication for secure sections of the platform.',
-        'Focused on responsive UI compatible with all devices (desktop, tablet, mobile).',
-        'Worked on SEO optimization for tool pages (dynamic meta tags, sitemap generation, robots.txt).',
-        'Ensured error handling and smooth user feedback with toasts, loaders, and validations.',
-        'Coordinated with backend team for performance optimizations and scaling strategies.'
-      ]
-    },
+  companies: any = [
+    // {
+    //   name: 'Fine Quick Tech Pvt Ltd',
+    //   icon: 'https://www.finequick.com/favicon.svg',
+    //   duration: 'July 2025 - Present (Part-time)',
+    //   degination: 'Software Engineer',
+    //   location: 'Greate Noida, Uttar Pradesh · Remote',
+    //   points: [
+    //     'Designed and developed the frontend interface using Angular with modular, component-driven architecture.',
+    //     'Created REST APIs using Spring Boot for dynamic tool rendering and processing.',
+    //     'Built interactive forms for input-heavy tools like plagiarism checkers, converters, and calculators.',
+    //     'Integrated third-party APIs (OCR, Translation, SEO APIs) with secure handling.',
+    //     'Managed file upload/download systems for tools like PDF converters and image converters.',
+    //     'Implemented JWT-based authentication for secure sections of the platform.',
+    //     'Implemented JWT-based authentication for secure sections of the platform.',
+    //     'Focused on responsive UI compatible with all devices (desktop, tablet, mobile).',
+    //     'Worked on SEO optimization for tool pages (dynamic meta tags, sitemap generation, robots.txt).',
+    //     'Ensured error handling and smooth user feedback with toasts, loaders, and validations.',
+    //     'Coordinated with backend team for performance optimizations and scaling strategies.',
+    //   ],
+    // },
     {
       name: 'Redmil Business (Rejoin)',
       icon: '/assets/images/redmil.jpeg',
-      duration: 'Sep 2024 - Present',
-      degination: "Full Stack Developer",
+      duration: 'Sep 2024 - Aug 2025',
+      degination: 'Full Stack Developer',
       location: 'Sector 142 Noida, Uttar Pradesh · On-site',
       points: [
         'Integrated fintech services like APES, Aadhaar Pay, DMT, Micro-ATM, Recharge, and Bill Payment APIs for seamless user transactions.',
@@ -60,31 +59,31 @@ export class Experience {
         'Managed secure communication between frontend and backend, including token handling, headers, and encrypted data transmission.',
         'Worked with Java Spring Boot backend when required, especially for encryption, decryption, and sensitive data handling.',
         'Implemented admin-level controls for managing agents, transaction approvals, and report generation.',
-        'Handled full-stack feature development, from UI/UX to API integration, testing, and deployment, with a focus on scalability.'
-      ]
+        'Handled full-stack feature development, from UI/UX to API integration, testing, and deployment, with a focus on scalability.',
+      ],
     },
-    {
-      name: 'WebConvoy Pvt Ltd',
-      icon: '/assets/images/webconvoy.jpeg',
-      duration: 'May 2024 - Aug 2024',
-      degination: "Angular Developer",
-      location: 'H-159, Sector 63, Noida, Uttar Pradesh, India · On-site',
-      points: [
-        'Designed responsive and interactive UI for core modules like login, registration, top-up, account, and transactions.',
-        'Integrated REST APIs for user authentication, transaction processing, and player management.',
-        'Developed QR code generation features for seamless payment flows.',
-        'Created dynamic car and product listing pages driven by data from backend APIs.',
-        'Integrated REST APIs for real-time content management (car listings, product details, blogs).',
-        'Developed admin functionalities for adding and updating products and blogs.',
-        'Ensured SEO-friendly structure with dynamic meta titles and descriptions.',
-        'Collaborated with designers and backend developers to optimize user experience.'
-      ]
-    },
+    // {
+    //   name: 'WebConvoy Pvt Ltd',
+    //   icon: '/assets/images/webconvoy.jpeg',
+    //   duration: 'May 2024 - Aug 2024',
+    //   degination: "Angular Developer",
+    //   location: 'H-159, Sector 63, Noida, Uttar Pradesh, India · On-site',
+    //   points: [
+    //     'Designed responsive and interactive UI for core modules like login, registration, top-up, account, and transactions.',
+    //     'Integrated REST APIs for user authentication, transaction processing, and player management.',
+    //     'Developed QR code generation features for seamless payment flows.',
+    //     'Created dynamic car and product listing pages driven by data from backend APIs.',
+    //     'Integrated REST APIs for real-time content management (car listings, product details, blogs).',
+    //     'Developed admin functionalities for adding and updating products and blogs.',
+    //     'Ensured SEO-friendly structure with dynamic meta titles and descriptions.',
+    //     'Collaborated with designers and backend developers to optimize user experience.'
+    //   ]
+    // },
     {
       name: 'Redmil Business Mall',
       icon: '/assets/images/redmil.jpeg',
       duration: 'Apr 2022 - Apr 2024',
-      degination: "Angular Developer",
+      degination: 'Angular Developer',
       location: 'Sector 62 Noida, Uttar Pradesh, India · On-site',
       points: [
         'Designed and developed a modular, component-driven architecture using Angular for scalability and maintainability.',
@@ -92,22 +91,22 @@ export class Experience {
         'Developed data filtering, search, and export features for enhanced reporting and data analysis.',
         'Ensured code quality, reusability, and optimization by adhering to Angular best practices.',
         'Utilized RxJS for effective state management and reactive programming patterns.',
-        'Handled full-stack feature development, from UI/UX to API integration, testing, and deployment, with a focus on scalability.'
-      ]
+        'Handled full-stack feature development, from UI/UX to API integration, testing, and deployment, with a focus on scalability.',
+      ],
     },
     {
       name: 'Caria Electronics',
       icon: 'assets/icons/emitrr.png',
       duration: 'Sep 2021 - Mar 2022',
-      degination: "Executive Assistant",
+      degination: 'Executive Assistant',
       location: 'B18, Block B, Sector 85, Noida, Uttar Pradesh',
       points: [
         'Prepare Handing Excel File as per requirement.',
         'Prepare Inventory as per requirement.',
         'Collection Data from Group.',
         'Prepare PIVOT Table as per requirement.',
-        'Prepare VLOOKUP Data sheet as per requirement.'
-      ]
+        'Prepare VLOOKUP Data sheet as per requirement.',
+      ],
     },
   ];
 
@@ -118,29 +117,45 @@ export class Experience {
   }
 
   setMetaTags(): void {
-    const pageTitle = 'Work Experience | Satendra Rajput - Angular & Java Full Stack Developer';
-    const pageDescription = 'Explore the professional work experience of Satendra Rajput across Fine Quick Solutions, Redmil, WebConvoy, and Caria Electronics. Hands-on roles in Angular, Java Spring Boot, SaaS platforms, fintech, and LMS systems.';
-    const imageUrl = 'https://satendrasde.vercel.app/assets/images/meta-experience.jpg'; // ✅ Replace with real image
+    const pageTitle =
+      'Work Experience | Satendra Rajput - Angular & Java Full Stack Developer';
+    const pageDescription =
+      'Explore the professional work experience of Satendra Rajput across Fine Quick Solutions, Redmil, WebConvoy, and Caria Electronics. Hands-on roles in Angular, Java Spring Boot, SaaS platforms, fintech, and LMS systems.';
+    const imageUrl =
+      'https://satendrasde.vercel.app/assets/images/meta-experience.jpg'; // ✅ Replace with real image
     const pageUrl = 'https://satendrasde.vercel.app/experience';
 
     this.titleService.setTitle(pageTitle);
 
     this.meta.updateTag({ name: 'description', content: pageDescription });
     this.meta.updateTag({ name: 'author', content: 'Satendra Rajput' });
-    this.meta.updateTag({ name: 'keywords', content: 'Satendra Rajput Work Experience, Fine Quick Solutions, Redmil Business, WebConvoy, Caria Electronics, Software Engineer, Angular Developer, Full Stack Developer, LMS Developer, Fintech Developer' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content:
+        'Satendra Rajput Work Experience, Fine Quick Solutions, Redmil Business, WebConvoy, Caria Electronics, Software Engineer, Angular Developer, Full Stack Developer, LMS Developer, Fintech Developer',
+    });
     this.meta.updateTag({ name: 'robots', content: 'index, follow' });
 
     // Open Graph
     this.meta.updateTag({ property: 'og:title', content: pageTitle });
-    this.meta.updateTag({ property: 'og:description', content: pageDescription });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: pageDescription,
+    });
     this.meta.updateTag({ property: 'og:image', content: imageUrl });
     this.meta.updateTag({ property: 'og:url', content: pageUrl });
     this.meta.updateTag({ property: 'og:type', content: 'website' });
 
     // Twitter Card
-    this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.meta.updateTag({
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    });
     this.meta.updateTag({ name: 'twitter:title', content: pageTitle });
-    this.meta.updateTag({ name: 'twitter:description', content: pageDescription });
+    this.meta.updateTag({
+      name: 'twitter:description',
+      content: pageDescription,
+    });
     this.meta.updateTag({ name: 'twitter:image', content: imageUrl });
   }
 }
--- src/app/pages/home/project/project.ts
+++ src/app/pages/home/project/project.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProjectCard } from "../project-card/project-card";
+import { ProjectCard } from '../project-card/project-card';
 import { CommonModule } from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser';
 interface Project_1 {
@@ -16,141 +16,217 @@ interface Project_1 {
   selector: 'app-project',
   imports: [ProjectCard, CommonModule],
   templateUrl: './project.html',
-  styleUrl: './project.scss'
+  styleUrl: './project.scss',
 })
 export class Project {
- constructor(private meta: Meta, private titleService: Title) {}
+  constructor(private meta: Meta, private titleService: Title) {}
 
   ngOnInit(): void {
     this.setMetaTags();
   }
 
-projects: Project_1[] = [
-  // 1_SSBrinet Innovations 
-   {
+  projects: Project_1[] = [
+    // 1_SSBrinet Innovations
+    {
       id: 1,
       title: 'Fine Quick Multi-Utility SaaS Platform',
-      description: 'Built Fine Quick Innovations, a SaaS platform with 150+ productivity tools using Angular & Spring Boot...',
+      description:
+        'Built Fine Quick Innovations, a SaaS platform with 150+ productivity tools using Angular & Spring Boot...',
       image: '/assets/images/projects/finequick_web/home.png',
-      technologies: ['HTML/CSS', 'Angular/TypeScript', 'Java', 'JWT', 'Spring Boot', 'MySQL'],
+      technologies: [
+        'HTML/CSS',
+        'Angular/TypeScript',
+        'Java',
+        'JWT',
+        'Spring Boot',
+        'MySQL',
+      ],
       category: 'Fine Quick Tech',
       demoUrl: 'https://www.finequick.com/',
-      codeUrl: 'https://github.com/satendracoder'
+      codeUrl: 'https://github.com/satendracoder',
     },
 
-    // 2_Redmil Fintech 
+    // 2_Redmil Fintech
     {
       id: 2,
       title: 'Fintech Platform',
-      description: 'Contributing to the development of a comprehensive Fintech platform offering a wide range of banking...',
+      description:
+        'Contributing to the development of a comprehensive Fintech platform offering a wide range of banking...',
       image: '/assets/images/projects/redmil_fintech/login.png',
-      technologies: ['HTML/CSS', 'Angular/TypeScript', 'Java', 'RESTful APIs', 'Spring Boot', 'MySQL'],
+      technologies: [
+        'HTML/CSS',
+        'Angular/TypeScript',
+        'Java',
+        'RESTful APIs',
+        'Spring Boot',
+        'MySQL',
+      ],
       category: 'Redmil',
       demoUrl: 'https://web.redmilbusinessmall.com/login',
-      codeUrl: 'https://github.com/satendracoder'
+      codeUrl: 'https://github.com/satendracoder',
     },
 
     // 3_Redmil HDFC
     {
       id: 3,
       title: 'Jan Samarth Referred Cases Dashboard (Angular)',
-      description: 'Developed a fully functional Angular dashboard for Jan Samarth referred case reporting and management...',
+      description:
+        'Developed a fully functional Angular dashboard for Jan Samarth referred case reporting and management...',
       image: '/assets/images/projects/redmil_hdfc/login.png',
-      technologies: ['HTML/CSS', 'JavaScript/TypeScript','Angular', 'Material UI'],
+      technologies: [
+        'HTML/CSS',
+        'JavaScript/TypeScript',
+        'Angular',
+        'Material UI',
+      ],
       category: 'Redmil',
       demoUrl: 'https://hdfc.betterbottech.ai/login',
-      codeUrl: 'https://github.com/satendracoder'
+      codeUrl: 'https://github.com/satendracoder',
     },
 
     // 4_Webconvoy Gaming Platform
     {
       id: 4,
       title: 'Online Gambling Platform',
-      description: 'Developed key frontend modules for an online gambling platform, including login, transactions, QR code payments, and player registration forms...',
+      description:
+        'Developed key frontend modules for an online gambling platform, including login, transactions, QR code payments, and player registration forms...',
       image: '/assets/images/projects/webconvoy_gaming/login.png',
-      technologies: ['HTML/CSS', 'JavaScript/TypeScript','Angular', 'Material UI'],
-      category: 'Webconvoy',
+      technologies: [
+        'HTML/CSS',
+        'JavaScript/TypeScript',
+        'Angular',
+        'Material UI',
+      ],
+      category: 'Redmil',
       demoUrl: 'https://poker9club.casino/',
-      codeUrl: 'https://github.com/satendracoder'
+      codeUrl: 'https://github.com/satendracoder',
     },
 
     // 5_Webconvoy PPF(Paint Protection Film)
     {
       id: 5,
       title: 'PPF(Paint Protection Film)',
-      description: 'Designed and developed a responsive and dynamic web application for showcasing and managing Paint Protection Film (PPF) services and products...',
+      description:
+        'Designed and developed a responsive and dynamic web application for showcasing and managing Paint Protection Film (PPF) services and products...',
       image: '/assets/images/projects/webconvoy_ppf/home.png',
-      technologies: ['HTML/CSS','JavaScript','TypeScript','Angular','Material UI','', 'MEAN Stack',],
-      category: 'Webconvoy',
+      technologies: [
+        'HTML/CSS',
+        'JavaScript',
+        'TypeScript',
+        'Angular',
+        'Material UI',
+        '',
+        'MEAN Stack',
+      ],
+      category: 'Redmil',
       demoUrl: 'https://eazycool.com.sg/',
-      codeUrl: 'https://github.com/satendracoder'
+      codeUrl: 'https://github.com/satendracoder',
     },
 
     //_6 Port Portfolio Website
     {
       id: 6,
       title: 'Personal Portfolio Website',
-      description: 'A personal portfolio website showcasing projects, skills, and professional experience.',
+      description:
+        'A personal portfolio website showcasing projects, skills, and professional experience.',
       image: '/assets/images/projects/portfolio_myself/homelight.png',
-      technologies: ['HTML','CSS','Angular', 'TypeScript', 'Material UI', 'Java Spring Boot', 'Rest APIs'],
+      technologies: [
+        'HTML',
+        'CSS',
+        'Angular',
+        'TypeScript',
+        'Material UI',
+        'Java Spring Boot',
+        'Rest APIs',
+      ],
       category: 'Collage & Myself',
       demoUrl: 'https://satendrasde.vercel.app/',
-      codeUrl: 'https://github.com/satendracoder'
+      codeUrl: 'https://github.com/satendracoder',
     },
 
     // Collage time Project
     {
       id: 7,
       title: 'Study Library (College Major Project)',
-      description: 'Study Library is an online educational platform designed to assist students, parents, and education stakeholders in accessing...',
+      description:
+        'Study Library is an online educational platform designed to assist students, parents, and education stakeholders in accessing...',
       image: '/assets/images/projects/lms_myself/home.jpg',
-      technologies: ['HTML', 'CSS','TypeScript','Angular_12', 'Material UI', 'Bootstrap'],
+      technologies: [
+        'HTML',
+        'CSS',
+        'TypeScript',
+        'Angular_12',
+        'Material UI',
+        'Bootstrap',
+      ],
       category: 'Collage & Myself',
       demoUrl: 'https://satendrasde.com/contact',
-      codeUrl: 'https://github.com/satendracoder'
-    }
+      codeUrl: 'https://github.com/satendracoder',
+    },
   ];
 
-  categories: string[] = ['All', 'Fine Quick Tech', 'Redmil', 'Webconvoy','Collage & Myself',];
+  categories: string[] = [
+    'All',
+    'Fine Quick Tech',
+    'Redmil',
+    'Webconvoy',
+    'Collage & Myself',
+  ];
   selectedCategory: string = 'All';
 
   filteredProjects(): Project_1[] {
     if (this.selectedCategory === 'All') {
       return this.projects;
     }
-    return this.projects.filter(project => project.category === this.selectedCategory);
+    return this.projects.filter(
+      (project) => project.category === this.selectedCategory
+    );
   }
 
   changeCategory(category: string): void {
     this.selectedCategory = category;
   }
 
-
-
   setMetaTags(): void {
-    const pageTitle = 'Projects by Satendra Rajput | Angular & Java Full Stack Developer';
-    const pageDescription = 'Explore major projects built by Satendra Rajput, including FineQuick SaaS, Fintech platforms, dashboards, PPF apps, and more using Angular, Java Spring Boot, and MySQL.';
-    const imageUrl = 'https://satendrasde.vercel.app/assets/images/meta-projects.jpg'; // ✅ Replace with actual thumbnail or banner
+    const pageTitle =
+      'Projects by Satendra Rajput | Angular & Java Full Stack Developer';
+    const pageDescription =
+      'Explore major projects built by Satendra Rajput, including FineQuick SaaS, Fintech platforms, dashboards, PPF apps, and more using Angular, Java Spring Boot, and MySQL.';
+    const imageUrl =
+      'https://satendrasde.vercel.app/assets/images/meta-projects.jpg'; // ✅ Replace with actual thumbnail or banner
     const pageUrl = 'https://satendrasde.vercel.app/projects';
 
     this.titleService.setTitle(pageTitle);
 
     this.meta.updateTag({ name: 'description', content: pageDescription });
     this.meta.updateTag({ name: 'author', content: 'Satendra Rajput' });
-    this.meta.updateTag({ name: 'keywords', content: 'Satendra Coder Projects, Angular Projects, Java Spring Boot Portfolio, Fintech Dashboard, SaaS Platform, FineQuick, Webconvoy, PPF, Gambling Platform, Jan Samarth Dashboard, Study Library' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content:
+        'Satendra Coder Projects, Angular Projects, Java Spring Boot Portfolio, Fintech Dashboard, SaaS Platform, FineQuick, Webconvoy, PPF, Gambling Platform, Jan Samarth Dashboard, Study Library',
+    });
     this.meta.updateTag({ name: 'robots', content: 'index, follow' });
 
     // Open Graph (for LinkedIn/Facebook sharing)
     this.meta.updateTag({ property: 'og:title', content: pageTitle });
-    this.meta.updateTag({ property: 'og:description', content: pageDescription });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: pageDescription,
+    });
     this.meta.updateTag({ property: 'og:image', content: imageUrl });
     this.meta.updateTag({ property: 'og:url', content: pageUrl });
     this.meta.updateTag({ property: 'og:type', content: 'website' });
 
     // Twitter Card
-    this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.meta.updateTag({
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    });
     this.meta.updateTag({ name: 'twitter:title', content: pageTitle });
-    this.meta.updateTag({ name: 'twitter:description', content: pageDescription });
+    this.meta.updateTag({
+      name: 'twitter:description',
+      content: pageDescription,
+    });
     this.meta.updateTag({ name: 'twitter:image', content: imageUrl });
   }
 }
